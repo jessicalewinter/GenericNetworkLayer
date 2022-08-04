a5cb5fa35ce735da2b94522ec92b4f8218258c87
Jessica Lewinter <jessicalewinter@gmail.com>
2022-08-04 16:22:14 -0300

    add: create jsonData method and convert encodable to dictionary

--- Sources/GenericNetworkLayer/Extensions/Encodable+asData.swift
+++ Sources/GenericNetworkLayer/Extensions/Encodable+asData.swift
@@ -9,29 +9,20 @@ extension Encodable {
         do {
             return try jsonEncoder.encode(self)
         } catch {
-            print("Failure to prepare card payload. \(error)")
+            print("Failure to prepare payload. \(error)")
             return nil
         }
     }
 }
 
-final class EncoderAdapter {
-    public var jsonEncoder: JSONEncodable
-    
-    init(jsonEncoder: JSONEncodable) {
-        self.jsonEncoder = jsonEncoder
-    }
-    
-    func prepareBody<T: Encodable>(with payload: T, strategy: JSONEncoder.KeyEncodingStrategy = .useDefaultKeys) -> Data? {
-//        let jsonEncoder = JSONEncoder()
-        jsonEncoder.outputFormatting = .prettyPrinted
-        jsonEncoder.keyEncodingStrategy = strategy
-
-        do {
-            return try jsonEncoder.encode(payload)
-        } catch {
-            print("Failure to prepare card payload. \(error)")
-            return nil
-        }
+extension Encodable {
+    /// Encode into JSON and return `Data`
+    public func jsonData(strategy: JSONEncoder.KeyEncodingStrategy = .useDefaultKeys) throws -> Data {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        encoder.dateEncodingStrategy = .iso8601
+        encoder.keyEncodingStrategy = strategy
+        
+        return try encoder.encode(self)
     }
 }
