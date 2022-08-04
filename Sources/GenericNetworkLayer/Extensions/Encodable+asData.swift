dcad5bc864ab94409558125c372e3dfc4fccdd47
Jessica Lewinter <jessicalewinter@gmail.com>
2022-08-04 16:26:51 -0300

    delete: remove datestrategy from encoder

--- Sources/GenericNetworkLayer/Extensions/Encodable+asData.swift
+++ Sources/GenericNetworkLayer/Extensions/Encodable+asData.swift
@@ -20,7 +20,6 @@ extension Encodable {
     public func jsonData(strategy: JSONEncoder.KeyEncodingStrategy = .useDefaultKeys) throws -> Data {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .prettyPrinted
-        encoder.dateEncodingStrategy = .iso8601
         encoder.keyEncodingStrategy = strategy
         
         return try encoder.encode(self)
